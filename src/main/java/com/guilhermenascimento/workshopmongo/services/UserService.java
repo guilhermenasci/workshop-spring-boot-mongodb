959f0b8e5cda8a7f17a22a50fcfb0039d20e6375
Guilherme Nascimento <guinascimento.job@gmail.com>
2022-02-23 16:26:33 -0300

    Get user by ID

--- src/main/java/com/guilhermenascimento/workshopmongo/services/UserService.java
+++ src/main/java/com/guilhermenascimento/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.guilhermenascimento.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilhermenascimento.workshopmongo.domain.User;
 import com.guilhermenascimento.workshopmongo.repository.UserRepository;
+import com.guilhermenascimento.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -14,8 +16,13 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 
-	public List<User> findAll(){ 
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
